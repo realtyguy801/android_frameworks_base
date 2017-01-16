30eb24b8da3f0ba8949787457888919d26cf9185
Shaun <shaunsibu@gmail.com>
2017-01-16 09:33:49 -0700

    Revert "logcat: fix SuperSU related spam"
    This reverts commit 723b75ccb3b4caafdc51e4412ae6a22c711e0abd.

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -4562,8 +4562,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                 if (actionName.startsWith("android.net.netmon.lingerExpired")
                         || actionName.startsWith("com.android.server.sip.SipWakeupTimer")
                         || actionName.startsWith("com.android.internal.telephony.data-reconnect")
-                        || actionName.startsWith("android.net.netmon.launchCaptivePortalApp")
-                        || actionName.startsWith("eu.chainfire.supersu.NativeAccess")) {
+                        || actionName.startsWith("android.net.netmon.launchCaptivePortalApp")) {
                     return true;
                 }
             }
