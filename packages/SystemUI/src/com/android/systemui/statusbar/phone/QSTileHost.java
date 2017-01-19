0088fb4b7f3e3765c0b061b4510d2795658f914a
ezio84 <brabus84@gmail.com>
2017-01-19 16:19:35 -0700

    QS: Smart Ambient-LiftToWake tile [1/2]
    Useful when cleaning up the device screen :D
    
    Now that N makes available two separate option for doze notifications
    (display-ambient and Gestures-LiftToWake) we need to handle them both
    with the tile.
    It will act this way:
    1) if any of the two options is enabled, and you tap the tile,
    both options are disabled so you can clean the screen or avoid
    unwanted disturbing wakes;
    2) if you tap the tile again, the tile will restore, separately,
    the two options state you had before disabling them;
    3) if you manually disable the two options, the tile will go to
     disabled state;
    4) if both options (thus the tile) are disabled, and you manually enable
    on of them, the tile will go to enabled state and act again as 1) and 2)
    
    Thanks Alex Cruz (@Mazda-- ) for the icons!
    
    Change-Id: I344a254faec9ba6ae47d3afceeb84f8175fec097

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -70,7 +70,7 @@ import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.AppCircleBarTile;
-import com.android.systemui.qs.tiles.AmbientDisplayTile;
+import com.android.systemui.qs.tiles.AmbientLiftToWakeTile;
 import com.android.systemui.qs.tiles.GestureAnyWhereTile;
 import com.android.systemui.qs.tiles.PulseTile;
 import com.android.systemui.qs.tiles.VolumeTile;
@@ -495,7 +495,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("app_circle_bar")) return new AppCircleBarTile(this);
         else if (tileSpec.equals("gesture_anywhere")) return new GestureAnyWhereTile(this);
         else if (tileSpec.equals("pie")) return new PieTile(this);
-        else if (tileSpec.equals("ambient_display")) return new AmbientDisplayTile(this);
+        else if (tileSpec.equals("ambient")) return new AmbientLiftToWakeTile(this);
         else if (tileSpec.equals("night")) return new NightDisplayTile(this);
         else if (tileSpec.equals("heads_up")) return new HeadsUpTile(this);
         else if (tileSpec.equals("locale")) return new LocaleTile(this);
