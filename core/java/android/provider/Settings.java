28ba14a114b9c9bd49732f47a71956b5d780acc4
xyyx <xyyx@mail.ru>
2017-02-08 11:02:12 -0700

    Unlock keystore with fingerprint after reboot (1/2)
    Change-Id: I36d58e9118c9bb53cdff69f267dbbb3959531418

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2737,6 +2737,12 @@ public final class Settings {
         public static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL =
                 "lockscreen_quick_unlock_control";
 
+         /**
+         * Unlock keystore with fingerprint after reboot
+         * @hide
+         */
+        public static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
+
         /**
          * @hide
          */
