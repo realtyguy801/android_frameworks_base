0dadeaa7caabdbcc58eab844edf887411f38394f
Mazda-- <mazdarider23@gmail.com>
2017-02-10 15:40:19 -0700

    Add check for flashlight API to RRUtils
    Change-Id: Ib3bd099f3c58bb89644e645d9ddb311d3b251712

--- core/java/com/android/internal/util/rr/RRUtils.java
+++ core/java/com/android/internal/util/rr/RRUtils.java
@@ -18,12 +18,37 @@ package com.android.internal.util.rr;
 
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
 import android.content.res.Resources;
 import android.net.ConnectivityManager;
 
 import java.util.Locale;
 
 public class RRUtils {
+
+    public static boolean deviceSupportsFlashLight(Context context) {
+        CameraManager cameraManager = (CameraManager) context.getSystemService(
+                Context.CAMERA_SERVICE);
+        try {
+            String[] ids = cameraManager.getCameraIdList();
+            for (String id : ids) {
+                CameraCharacteristics c = cameraManager.getCameraCharacteristics(id);
+                Boolean flashAvailable = c.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
+                Integer lensFacing = c.get(CameraCharacteristics.LENS_FACING);
+                if (flashAvailable != null
+                        && flashAvailable
+                        && lensFacing != null
+                        && lensFacing == CameraCharacteristics.LENS_FACING_BACK) {
+                    return true;
+                }
+            }
+        } catch (CameraAccessException e) {
+            // Ignore
+        }
+        return false;
+    }
 
     public static boolean isWifiOnly(Context context) {
         ConnectivityManager cm = (ConnectivityManager)context.getSystemService(
