c7c6b051b0b256565c76b9368abfda7fd92f3063
ezio84 <brabus84@gmail.com>
2017-03-01 23:49:45 +0100

    Slim Recent lastapp toggle: better app animation
    Change-Id: I26be23b253ec52c7fc542f56382de4033499136d

--- packages/SystemUI/src/com/android/systemui/slimrecent/RecentController.java
+++ packages/SystemUI/src/com/android/systemui/slimrecent/RecentController.java
@@ -20,6 +20,7 @@ package com.android.systemui.slimrecent;
 import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
 import android.app.ActivityManager.MemoryInfo;
+import android.app.ActivityOptions;
 import android.app.KeyguardManager;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
@@ -401,7 +402,7 @@ public class RecentController implements RecentPanelView.OnExitListener,
                     preloadRecentTasksList();
                 }
             } else {
-                openLastApptoBottom();
+                openLastAppPanelToggle();
                 hideRecents(false);
             }
         }
@@ -988,6 +989,20 @@ public class RecentController implements RecentPanelView.OnExitListener,
 
         if (lastTask != null) {
             am.moveTaskToFront(lastTask.id, ActivityManager.MOVE_TASK_NO_USER_ACTION);
+        }
+    }
+
+    public void openLastAppPanelToggle() {
+        final ActivityOptions animations = ActivityOptions.makeCustomAnimation(mContext,
+                mMainGravity == Gravity.LEFT ? com.android.internal.R.anim.recent_screen_enter_left :
+                com.android.internal.R.anim.recent_screen_enter,
+                com.android.internal.R.anim.recent_screen_fade_out);
+        final ActivityManager am =
+                (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
+        ActivityManager.RunningTaskInfo lastTask = getLastTask(am);
+
+        if (lastTask != null) {
+            am.moveTaskToFront(lastTask.id, ActivityManager.MOVE_TASK_NO_USER_ACTION, animations.toBundle());
         }
     }
 
